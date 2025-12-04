74ec1b520af28fc06210e6ec1a87345f2986a95f
Youssef Sarofeem <Youssef_Sarofeem@aucegypt.edu>
2025-12-04 20:58:19 +0200

    Merged borrow return part into main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "loginwindow.h"
 #include "usermanager.h"
-#include "bookrecords.h"
 #include <QApplication>
 #include <QFile>
 
@@ -8,8 +7,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    BookRecords records;
-    records.show();
+    UserManager userManager("Y:\\AUC\\CS II\\CS2 Lab\\Project\\Librarystem\\users.txt");
+    LoginWindow login(&userManager);
+    login.show();
 
     return a.exec();
 }
